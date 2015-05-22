cccd0073eb1d216cd2e6f898153a365777d863fa
Christopher <hello@blick-labs.com>
2015-05-22 11:09:45 -0700

    solve puzzle/new game, cleanup styling. move puzzle to state

--- index.ios.js
+++ index.ios.js
@@ -12,15 +12,15 @@ function onInput(key, value){
   var gridpoint = key.split('-');
   var x = gridpoint[0];
   var y = gridpoint[1];
-  puzzle = _.flatten(solved);
-  puzzle[x][y] = parseInt(value);
-  
-  if(_.flatten(puzzle).equals(solved)) {
-    cosole.log('puzzle Solved!!!!')
-  }
+  puzzle[x][y] = parseInt(--value);
+
+  if(Sudoku.boardmatches(_.flatten(puzzle), solved)){
+    console.log('Game Solved');
+  } 
 }
 console.log(_.flatten(puzzle));
-console.log(solved);
+console.log(_.chunk(solved, 9));
+
 var {
   AppRegistry,
   StyleSheet,
@@ -32,18 +32,29 @@ var {
   View,
 } = React;
 
-var Sudoku = React.createClass({
-  componentDidMount(){
-    this.generateBoard();
+var SudokuGame = React.createClass({
+  getInitialState() {
+    return {
+      puzzle: _.flatten(puzzle)
+    }
   },
 
   _onInput(input) {
     console.log(input);
   },
 
+  newGame() {
+    this.setState({puzzle: Sudoku.makepuzzle()});
+  },
+
+  solvePuzzle() {
+    this.setState({puzzle: solved});
+  },
+
   generateBoard() {
     var rows = [];
     var blocks = [];
+    var puzzle = _.chunk(this.state.puzzle, 9);
 
     puzzle.map(function(row){
       var rowSeperator = ((rows.length == 2 || rows.length == 5)) ? true : false;
@@ -59,14 +70,14 @@ var Sudoku = React.createClass({
                 clearTextOnFocus={true}
                 keyboardType={'number-pad'}
                 style={styles.textInput}
-                onChangeText={(input) => onInput(key, input)}
+                onChangeText={(input) => this._onInput(input)}
               />
             </View>
           );
         } else {
           blocks.push(
-            <View key={key} style={styles.block}>
-              <Text style={styles.blockText}>{block}</Text>
+            <View key={key} style={[styles.block, blockSeperator && styles.blockSeperator]}>
+              <Text style={styles.blockText}>{++block}</Text>
             </View>
           );
         }
@@ -79,8 +90,12 @@ var Sudoku = React.createClass({
 
   render() {
     return (
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Sudoku</Text>
+      <View>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={this.solvePuzzle}><Text>Solve Puzzle</Text></TouchableOpacity>
+          <Text style={styles.headerText}>Sudoku</Text>
+          <TouchableOpacity onPress={this.newGame}><Text>New Game</Text></TouchableOpacity>
+        </View>
         <View style={styles.container}>
           {this.generateBoard()}
         </View>
@@ -91,18 +106,20 @@ var Sudoku = React.createClass({
 
 var styles = StyleSheet.create({
   header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: 25,
-    marginBottom: 20,
-    alignSelf: 'center',
+    marginBottom: 10,
+    paddingLeft: 25,
+    paddingRight: 25,
   },
   headerText:{
     textAlign: 'center',
     fontSize: 20,
-    marginBottom: 15,
   },
   container: {
     alignSelf: 'center',
-    width:300,
+    width:320,
     borderWidth: 3,
     borderTopWidth: 2,
     borderBottomWidth: 2
@@ -138,4 +155,4 @@ var styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('Sudoku', () => Sudoku);
+AppRegistry.registerComponent('Sudoku', () => SudokuGame);
